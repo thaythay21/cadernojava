e563f48cdaf4da48c3ff6c87f0af4dec7ca23e7a
thaythay21 <131307487+thaythay21@users.noreply.github.com>
2023-05-24 17:52:33 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo");
+    int num = 5;
+    System.out.println(num);
+    int myNum = -5;
+float myFloatNum = 2143.456f;
+char myLetter = 'a';
+boolean myBool = false;
+String myText = "Olá";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+    //prate 2 do codigo
+    System.out.println(myText + myBool);
+    int x = 5;
+    float y = x;
+     System.out.println(x);
+    System.out.println(y);
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
+
+  
+  
+}
