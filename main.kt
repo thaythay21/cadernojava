41a700bc4a226022076607613e8c8f9f87468f49
thaythay21 <131307487+thaythay21@users.noreply.github.com>
2023-05-26 16:16:49 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,17 @@
+fun main() {
+  println("Hello World")
+  var name = "John"
+  val birthyear = 1975
+  println(name)          // Print the value of name
+  println(birthyear)     // Print the value of birthyear
+  val myNum = 5// Int
+  println(myNum)
+  val myDoubleNum = 5.99    // Double
+  println(myDoubleNum)
+  val myLetter = 'D'        // Char
+  println(myLetter)
+  val myBoolean = true      // Boolean
+  println(myBoolean)
+  val myText = "Hello"      // String
+  println(myText)
+}
